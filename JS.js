9dbb2c43423f4008c25b52d0c5cf5a82fec141c4
AuDietrich <33529775+AusDietrich@users.noreply.github.com>
2022-07-19 23:29:55 -0700

    Update JS.js
    innerhtml

--- JS.js
+++ JS.js
@@ -3,6 +3,6 @@ function contact(piece) {
 	setTimeout(otherMethod(piece), 1000);
 }
 function otherMethod(piece) {
-	document.getElementById('contact').value = "I would be interested in " + piece;
+	document.getElementById('contact').innerHTML = "I would be interested in " + piece;
 	document.getElementById('custom').checked = true;
 }

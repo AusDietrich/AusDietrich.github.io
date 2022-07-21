0fb202d69f13738be191ae49b8fb8d253372b604
AuDietrich <33529775+AusDietrich@users.noreply.github.com>
2022-07-20 17:08:52 -0700

    Update JS.js

--- JS.js
+++ JS.js
@@ -1,10 +1,7 @@
 
-let textBox = document.getElementById('contact');
-let checkedBox = document.getElementById('custom');
-
 function contact(piece) {
 	window.location.href = "/contact";
-    textBox.value = "I would be interested in " + piece;
+    document.getElementById('contact').value = "I would be interested in " + piece;
     console.log(textBox)
-    checkedBox.checked = true;
+     document.getElementById('custom').checked = true;
 }

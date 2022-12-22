67fa2194beb62fe86eec7828b0190e58e55a53e8
AuDietrich <33529775+AusDietrich@users.noreply.github.com>
2022-12-21 20:37:21 -0700

    Update JS.js

--- JS.js
+++ JS.js
@@ -11,9 +11,10 @@ function contact(piece) {
 function formFill(){
 	console.log(sessionStorage.getItem("interest"));
 	console.log(sessionStorage.getItem("checkBox"));
+	var piece = sessionStorage.getItem("interest");
 	if(!(sessionStorage.getItem("interest") == null)){
-		document.getElementById("contact").innerText = "I would be interested in " ;
-		document.getElementById("contact2").innerText = "I would be interested in " ;
+		document.getElementById("contact").innerText = "I would be interested in "+piece;
+		document.getElementById("contact2").innerText = "I would be interested in "+piece+".";
 	}
 	document.getElementById("custom").checked = sessionStorage.getItem("checkBox");
 	document.getElementById("custom2").checked = sessionStorage.getItem("checkBox");

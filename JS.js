5af4b9ca06344760f941fe9fd6463125725eb836
AuDietrich <33529775+AusDietrich@users.noreply.github.com>
2022-12-21 18:33:20 -0700

    changed name

--- JS.js
+++ JS.js
@@ -3,14 +3,15 @@
 function contact(piece) {
 	window.location.href = "/contact";
 	sessionStorage.setItem("checkBox", true);
-	sessionstorage.setItem("piece", piece);
-	console.log(sessionStorage.getItem("piece"));
+	sessionstorage.setItem("interest", piece);
+	console.log(sessionStorage.getItem("interest"));
 	console.log(sessionStorage.getItem("checkBox"));
+	console.log(piece);
 }
 function formFill(){
-	console.log(sessionStorage.getItem("piece"));
+	console.log(sessionStorage.getItem("interest"));
 	console.log(sessionStorage.getItem("checkBox"));
-    document.getElementById('contact').value = "I would be interested in " + sessionStorage.getItem("piece");
+    document.getElementById('contact').value = "I would be interested in " + sessionStorage.getItem("interest");
     document.getElementById('custom').checked = sessionStorage.getItem("checkBox");
 }
 
